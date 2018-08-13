865c00446177dedf21e83f11ccb9b4cd145b0fcf
Min Lee <minho.lee.93@hotmail.com>
2018-08-12 21:48:59 -0400

    adding few more test cases in 2 playlists

--- app.js
+++ app.js
@@ -101,14 +101,26 @@ app.get('/v1/browse/playlists/:playlistid/tracks', function(request, response) {
         response.send(JSON.stringify({
             "title": "Elon Musk is trending",
             "media": "http://via.placeholder.com/350x350",
-            "articles": [{
-                "id": "article_0",
-                "headline": "sukh",
-                "abstract": "This is an abstract",
-                "publisher": "NY Times",
-                "media": "http://via.placeholder.com/100x100",
-                "url": "http://www.google.com"
-            }]
+            "articles": [
+                {
+                    "order": 0,
+                    "id": "da3af41fcf929d452155a2ea7b6e6905",
+                    "headline": "Cops: Man stabs, runs over estranged girlfriend, killing her",
+                    "abstract": "A man stabbed and then repeatedly drove over his estranged girlfriend, crushing her in a fatal attack as her suburban Philadelphia co-workers tried to help her early Friday morning, authorities said.",
+                    "publisher": "Fox News",
+                    "media": "http://global.fncstatic.com/static/orion/styles/img/fox-news/og/og-fox-news.png",
+                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b708023fbb4f65330270ca0"
+                },
+                {
+                    "order": 1,
+                    "id": "44dc8f793c8314433ea03b1790012087",
+                    "headline": "Urban Meyer says he didn't handle Zach Smith questions right",
+                    "abstract": "Ohio State coach Urban Meyer has issued a statement saying that he \"failed\" when he said he didn't know about a series of domestic violence cases involving his ex-assistant coach Zach Smith.",
+                    "publisher": "Espn.com",
+                    "media": "http://a4.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0802%2Fr409314_1296x729_16%2D9.jpg",
+                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b708022fbb4f65330270c94"  
+                }
+            ]
         }))
     } else if (request.params.playlistid === "d2d124") {
         response.send(JSON.stringify({
@@ -117,30 +129,34 @@ app.get('/v1/browse/playlists/:playlistid/tracks', function(request, response) {
             "articles": [
                 {
                     "order": 0,
-                    "id": "a5eb5246017772398285696db94cedb1",
-                    "headline": "The Latest: Relatives paint inconsistent picture of gunman",
-                    "abstract": "The Latest on the final report into the Oct. 1 mass shooting in Las Vegas that killed 58 people (all times local): 10:05 a.m.",
-                    "publisher": "Fox News",
-                    "media": "http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2018/08/03/latest-brother-vegas-shooter-says-was-delusional/_jcr_content/par/featured-media/media-0.img.png/0/0/1533319090154.png?ve=1",
-                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b69021044ea3b11a1e1d296"
+                    "id": "cd641bde4a9bd06fe14b496d0d4959df",
+                    "headline": "Portland's Patriot Prayer Rally Could Be Most Violent Since Charlottesville, Activists Say",
+                    "abstract": "The last far-right rally by Proud Boys and Patriot Prayer was declared a riot. Experts think this weekend could be even worse than 2017's deadly \"Unite the Right\" event.",
+                    "publisher": "The Huffington Post",
+                    "media": "https://img.huffingtonpost.com/asset/5b64693d19000016035010ba.jpeg?ops=1200_630",
+                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b708022fbb4f65330270c9c"
+                    // "backend": 'http://www.noiseaddicts.com/samples_1w72b820/291.mp3'
                 },
                 {
                     "order": 1,
-                    "id": "5fd26a045b1520432e447125feb14ef1",
-                    "headline": "Northern California wildfire created destructive tornado",
-                    "abstract": "Get breaking national and world news, broadcast video coverage, and exclusive interviews. Find the top news online at ABC news.",
-                    "publisher": "ABC News",
-                    "media": "https://s.abcnews.com/images/US/WireAP_76a73daf54124ac5b05307bbd15b404e_16x9_992.jpg",
-                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b69021044ea3b11a1e1d295"
+                    "id": "77cbdc06dab399704dedb50b15343af8",
+                    "headline": "12-year-old girl at center of Amber Alert found safe in New York City",
+                    "abstract": "JinJing Ma was thought to be in danger after she was last seen Thursday leaving Reagan Washington National Airport. She was in the U.S. from China.",
+                    "publisher": "USA Today",
+                    "media": "https://www.gannett-cdn.com/-mm-/0fb854c5fc292bf8e9ea056c9dc3928ca198d7bf/c=0-62-2000-1192/local/-/media/2018/08/03/USATODAY/USATODAY/636688801695140638-airport-abduction-080318.JPG?width=3200&height=1680&fit=crop",
+                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b708022fbb4f65330270c9e"
+                    
+                    // "backend": 'http://www.noiseaddicts.com/samples_1w72b820/4940.mp3'
                 },
                 {
-                    "order": 2,
-                    "id": "bb2e08eb19db97469ab593ce4b454755",
-                    "headline": "Iraq War veteran's wife deported to Mexico after wanting Trump to intervene",
-                    "abstract": "Alejandra Juarez, 39, who had been living in Davenport, Florida, had an emotional goodbye with her family Friday, her attorney said.",
-                    "publisher": "NBC News",
-                    "media": "https://media2.s-nbcnews.com/j/MSNBC/Components/Video/201808/f_tov_juarez_180801_wip.1200;630;7;70;5.jpg",
-                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b69021144ea3b11a1e1d29d"
+                    "order": 2,                
+                    "id": "45dafebf553fc45bfa76d0c885263d43",
+                    "headline": "Missing University of Iowa student's boyfriend speaks out: 'Just imagine if somebody had taken your Mollie'",
+                    "abstract": "It's been more than two weeks since Dalton Jack last saw or heard from his girlfriend, 20-year-old Mollie Tibbetts, before she mysteriously vanished in a rural Iowa town.",
+                    "publisher": "ABC News",
+                    "media": "https://s.abcnews.com/images/US/missing-iowa-mollie-tibbetts-ht-mem-180723_hpMain_7_v2x1_16x9_992.jpg",
+                    "backend": "https://newseon-backend-api.herokuapp.com/tracks/5b708023fbb4f65330270ca5"
+                    // "backend": 'http://techslides.com/demos/samples/sample.mp3'
                 }
                 
             ]
